336878a5f18085f3cce3652fd6591c1fde9cd4dc
Andrey Amelchenia <pmad@tut.by>
2020-03-22 17:58:15 +0300

    docs: index Ok

--- src/index.js
+++ src/index.js
@@ -38,7 +38,60 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    expr = expr.split('');
+    arr = [];
+    arr1 = [];
+    while(expr.length != 0){
+        for (let i=0; i<10; i++){
+            arr.push(expr[i]);
+        }
+        arr1.push(arr);
+        arr = [];
+        if (expr.length != 0){
+            expr = expr.splice(10)
+        }
+    }
+
+    const arr2 = [];
+    let arr3 = [];
+    arr1.forEach(el => {
+        for (let i = 0; i<10; i+=2){
+            arr3.push(el[i] + el[i+1])
+        }
+        arr2.push(arr3);
+        arr3 = [];
+
+    });
+
+    let cod = '';
+    let arrEnd = [];
+    arr2.forEach(el =>{
+        for (let i = 0; i<5; i++){
+            switch(el[i]){
+                case '10':
+                    cod = cod + '.'
+                    break
+                case '11':
+                    cod = cod + '-'
+                    break
+                case '**':
+                    cod = ' '
+                    break
+            }
+        }
+        arrEnd.push(cod);
+        cod = ''
+    })
+
+    let str = '';
+    arrEnd.forEach(el => {
+       if(el != ' ') {
+       str = str + MORSE_TABLE[el]; 
+       } else{
+        str = str + el; 
+       }
+    })
+    return str
 }
 
 module.exports = {
